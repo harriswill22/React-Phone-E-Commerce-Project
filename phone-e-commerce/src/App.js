1a7a8e9d956d6a16b691e8ef5e43cbbf8c2bf31b
Will Harris <Harriswill22@yahoo.com>
2019-04-03 11:13:53 -0400

    crates cart component

--- phone-e-commerce/src/App.js
+++ phone-e-commerce/src/App.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Navbar from './components/Navbar';
+import Default from './components/Defualt'
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
 class App extends Component {
   render() {
     return (
     <React.Fragment>
-      <h3>Hello from app</h3>
+      <Navbar></Navbar>
+      <ProductList></ProductList>
+      <Details></Details>
+      <Cart></Cart>
+      <Default></Default>
     </React.Fragment>
     );
   }
