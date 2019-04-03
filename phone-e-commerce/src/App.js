058a79ae83820d74950ffe8edc7f79af4892bebd
Will Harris <Harriswill22@yahoo.com>
2019-04-03 12:04:19 -0400

    creates exact route

--- phone-e-commerce/src/App.js
+++ phone-e-commerce/src/App.js
@@ -16,14 +16,11 @@ class App extends Component {
     <React.Fragment>
       <Navbar/>
       <Switch>
-        <Route path="/" component={ProductList}/>
+        <Route exact path="/" component={ProductList}/>
         <Route path="/details" component={Details}/>
         <Route path="/cart" component={Cart}/>
-        <Route component={Defualt}/>
+        <Route component={Default}/>
       </Switch>
-      <Details></Details>
-      <Cart></Cart>
-      <Default></Default>
     </React.Fragment>
     );
   }
